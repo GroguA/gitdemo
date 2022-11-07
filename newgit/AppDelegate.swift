d2ab4189b80b2da4c6c950390620cc67d18421ec
GroguA <117662071+GroguA@users.noreply.github.com>
2022-11-07 18:31:00 +0300

    Update AppDelegate.swift

--- newgit/AppDelegate.swift
+++ newgit/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        print("hello world")
         // Insert code here to initialize your application
     }
 
