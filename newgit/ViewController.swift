18f1c828e26382b4b9616cf3e48dc9dd3bcf663f
sasha sergeeva <moyemail@gmail.com>
2022-11-07 17:30:15 +0300

    added gitignore

---
+++ newgit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  newgit
+//
+//  Created by Александра Сергеева on 07.11.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
